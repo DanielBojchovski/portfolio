b560947ef01666ba2d72543b98c5b6e60a3a1b10
Your Name <you@example.com>
2023-03-04 23:45:22 +0100

    create home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,40 @@
 export default function Home() {
   return (
-    <h1 className='text-red-400'>Home</h1>
-  )
+    <div className="grid h-screen place-items-center">
+      <div className="max-w-6xl mx-auto space-y-4 p-4 ">
+        <h1 className="text-4xl font-medium text-amber-600">Hi there,</h1>
+        <div className="text-xl">
+          <p>
+            I'm Daniel, a{" "}
+            <span className="text-3xl text-amber-600 font-medium">
+              Software Developer
+            </span>{" "}
+            based in Skopje, Macedonia. I have a passion for building beautiful
+            and user-friendly applications, and I have hands-on experience in
+            developing various types of software.
+          </p>
+          <br />
+          <p>
+            As examples, you can find links to some of my applications,
+            including an{" "}
+            <span className="text-2xl text-amber-600 font-medium">
+              IMDB app, a ToDo app and a Credit/Loan Calculator
+            </span>{" "}
+            in the navigation bar of this website.
+          </p>
+          <br />
+          <p>
+            I have a solid understanding of both front-end and back-end
+            development, and I'm excited to put my skills to use in a
+            professional setting. As a{" "}
+            <span className="text-2xl text-amber-600 font-medium">
+              quick learner and a team player
+            </span>
+            , I am confident that I can be a valuable asset to any software
+            development team.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
